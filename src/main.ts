a7c4aa8eb81cc3d46e8bb455bd464392db65e1af
Link <h26281660@gmail.com>
2024-02-08 20:37:08 +0800

    complete basic font size change functions

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-import './style.css';
+import '~/style.css';
+import { getHtmlElement } from '~/utils';
+import { modifyFontSize } from '~/funcs';
+import { inertScrtiptToActiveTab } from '~/chromeService';
+
+const increaseBtnEl = getHtmlElement<HTMLButtonElement>('#increaseBtn');
+
+increaseBtnEl.addEventListener('click', async () => {
+  inertScrtiptToActiveTab(modifyFontSize, ['increase']);
+});
+
+const decreaseBtnEl = getHtmlElement<HTMLButtonElement>('#decreaseBtn');
+
+decreaseBtnEl.addEventListener('click', async () => {
+  inertScrtiptToActiveTab(modifyFontSize, ['decrease']);
+});
