087bfb20a187f01a6fd452f49b9190dbe989303c
Link <h26281660@gmail.com>
2024-02-12 11:55:04 +0800

    add tabs permission

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,16 @@
 import '~/style.css';
 import { getHtmlElement } from '~/utils';
 import { modifyFontSize } from '~/funcs';
-import {
-  // insertScrtiptToActiveTab,
-  inertScrtiptToAllTabs,
-} from '~/chromeService';
+import { inertScrtiptToAllTabs } from '~/chromeService';
 
 const increaseBtnEl = getHtmlElement<HTMLButtonElement>('#increaseBtn');
 
 increaseBtnEl.addEventListener('click', async () => {
-  // insertScrtiptToActiveTab(modifyFontSize, ['increase']);
   inertScrtiptToAllTabs(modifyFontSize, ['increase']);
 });
 
 const decreaseBtnEl = getHtmlElement<HTMLButtonElement>('#decreaseBtn');
 
 decreaseBtnEl.addEventListener('click', async () => {
-  // insertScrtiptToActiveTab(modifyFontSize, ['decrease']);
   inertScrtiptToAllTabs(modifyFontSize, ['decrease']);
 });
