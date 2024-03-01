261ff054f6c596eb0d912a68fd92e3e312b0f786
Link1515 <h26281660@gmail.com>
2024-03-01 16:21:20 +0800

    add zoom in animation when init

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,5 +1,17 @@
 const zoom = localStorage.getItem('zoomHelperZoom');
 
 if (zoom) {
-  document.body.style.zoom = zoom;
+  const duration = 500;
+
+  document.body.style.transition = `scale ${duration}ms`;
+  document.body.style.transformOrigin = 'top';
+  document.body.style.scale = zoom;
+
+  setTimeout(() => {
+    document.body.style.transition = '';
+    document.body.style.scale = '';
+    document.body.style.transformOrigin = '';
+
+    document.body.style.zoom = zoom;
+  }, duration);
 }
